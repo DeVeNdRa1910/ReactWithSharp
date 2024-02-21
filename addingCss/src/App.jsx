c85fc7550586acaf4cdb7370c337e76922d8f969
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-02-21 19:07:04 +0530

    add 100 component in App.jsx

--- addingCss/src/App.jsx
+++ addingCss/src/App.jsx
@@ -5,11 +5,39 @@ import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let expensesArr = [
+    {
+      id: 1,
+      title: "car insurance",
+      date: "2024-2-21",
+      amount: "₹8999/-",
+      location: "BHOPAL"
+    }
+  ]
+  for (let i = 2; i <= 100; i++){
+    let temp = {
+      id: i,
+      title: "car insurance",
+      date: "2024-2-21",
+      amount: Math.floor(Math.random()*10000),
+      location: "BHOPAL"
+    }
+    expensesArr.push(temp);
+  }
+  console.log(expensesArr);
   return (
     <>
     <h1>Let's get started!</h1>
-      <ExpenseItem />
+      {
+        expensesArr.map(exp=>(
+          <ExpenseItem
+            location={exp.location}
+            title={exp.title}
+            amount={exp.amount}
+            date={exp.date}
+          />
+        ))
+      }
     </>
   )
 }
--- addingCss/src/components/ExpenseItem.jsx
+++ addingCss/src/components/ExpenseItem.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2024 , 2 , 21);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 8999
-    const expenseLocation = "BHOPAL"
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <h2>{expenseLocation}</h2>
-        <div className='expense-item__price'>₹{expenseAmount}/-</div>
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className='expense-item__price'>₹{props.amount}/-</div>
       </div>
     </div>
   )
