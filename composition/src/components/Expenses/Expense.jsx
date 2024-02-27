af8b7a8c1d9376557766a4473c384feeba3b59e8
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-02-27 14:53:00 +0530

    apply filter

--- composition/src/components/Expenses/Expense.jsx
+++ composition/src/components/Expenses/Expense.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Expense.css'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 function Expense(props) {
   // props.items.foeEach((expense) => { // Use map instead of forEach
   //       console.log(expense.title , expense.amount);
   // })
-  return (
-    <Card className="expense">
 
+  const [filteredYear , setFilteredYear] = useState('2020')
+  let filteredArray = [];
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onApplyFilter={filterChangeHandler} />
       {props.items.map((expense) => ( // Use map instead of forEach
         <ExpenseItem
           key={expense.id} // Add a unique key prop for each item
