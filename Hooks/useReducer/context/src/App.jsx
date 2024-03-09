954efecb0266355d3ee714fc5603758cb3ae367c
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-03-09 21:56:21 +0530

    Learning of Context API

--- Hooks/useReducer/context/src/App.jsx
+++ Hooks/useReducer/context/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Login from './Components/Login/Login';
 import Home from './Components/Home/Home';
 import MainHeader from './Components/MainHeader/MainHeader';
+import AuthContext from './context/authContext';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,13 +19,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+    </AuthContext.Provider>
   );
 }
 
