c713c1a4e5290d11bda67e43f0452632173b6a4f
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-03-29 18:04:26 +0530

    show the food item which available in restaurent

--- Restaurent web/reastaurentWeb/src/Components/UI/InputField.jsx
+++ Restaurent web/reastaurentWeb/src/Components/UI/InputField.jsx
@@ -4,7 +4,7 @@ function InputField(props) {
   return (
     <div>
       <label className='mr-2' htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} className='bg-slate-300 py-2 px-2 rounded-2xl shadow-2xl border-2 hover:border-black'/>
+      <input {...props.input} className='bg-slate-200 py-2 px-2 rounded-2xl shadow-2xl border-2 hover:border-black'/>
     </div>
   )
 }
