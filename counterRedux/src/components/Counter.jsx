1e273c6d3b963896beab85f2e25b9660ae6a1ae0
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2024-04-22 23:10:53 +0530

    using redux toolkit

--- counterRedux/src/components/Counter.jsx
+++ counterRedux/src/components/Counter.jsx
@@ -1,11 +1,19 @@
-import { useDispatch, useSelector, connect } from "react-redux"; 
-import { Component } from "react";
+import { useDispatch, useSelector } from "react-redux"; 
+import { useRef } from "react";
 
+// type id known as identifire
+
+// import { INCREMENT, DECREMENT } from "../store";
 
 function Counter() {
+
+    const inputRef = useRef(0);
     
+    console.log(typeof inputNumber);
+
     const dispatch = useDispatch();
     const counter = useSelector(state => state.counter);
+    const show = useSelector(state => state.showCounter)
 
     const incrementHandler = () => {
         dispatch({type: 'increment'})
@@ -16,27 +24,34 @@ function Counter() {
     }
 
     const decreaseHandler = () => {
-        dispatch({type: 'decrease', amount: 50})
+        let inputNumber = parseInt(inputRef.current.value);
+        return dispatch({type: 'decrease', amount: inputNumber})
     }
 
     const increaseHandler = () => {
-        dispatch({type: 'increase', amount: 500})
+        let inputNumber = parseInt(inputRef.current.value);
+        return dispatch({type: 'increase', amount: inputNumber})
     }
 
-    const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+        dispatch({type: 'toggle'})
+    };
 
   return (
     <main className="flex justify-center ">
-        <div className="bg-stone-100 w-3/5 p-6 rounded-2xl mt-20">
+        <div className="bg-stone-200 w-3/5 p-6 rounded-2xl mt-20">
             <h1 className="text-center font-bold">Redux Counter</h1>
-            <div className="text-center my-3 text-4xl font-bold">{counter}</div>
+            <div className="text-center my-3 text-4xl font-bold">
+                <input ref={inputRef} defaultValue={0} className="text-center font-bold border-2 border-black" type="number" />
+            </div>
+            {show && <div className="text-center my-3 text-4xl font-bold">{counter}</div>}
             <div className="flex justify-around pt-4 px-20">
                 <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={incrementHandler} >Increment</button>
                 <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={decrementHandler} >Decrement</button>
             </div>
             <div className="flex justify-around pt-4 px-20">
-                <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={increaseHandler} >IncrementBy5</button>
-                <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={decreaseHandler} >DecrementBy5</button>
+                <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={increaseHandler} >Increment By</button>
+                <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={decreaseHandler} >Decrement By</button>
             </div>
             <div className="flex justify-center py-8">
                 <button className="border border-stone-950 text-stone-900 hover:bg-black hover:text-white py-1.5 px-8 rounded-xl" onClick={toggleCounterHandler} >Toggle Counter</button>
--- counterRedux/src/store/index.js
+++ counterRedux/src/store/index.js
@@ -1,30 +1,104 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type === 'increment') {
-        return {
-            counter : state.counter + 1
-        }
-    }
-    if(action.type === 'increase') {
-        return {
-            counter : state.counter + action.amount
-        }
-    }
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if(action.type === 'decrease') {
-        return {
-            counter: state.counter - action.amount
-        }
-    }
-
-    return state;
+const initialState = {
+    counter: 0,
+    showCounter: true,
 }
 
-const store = createStore(counterReducer);
+// identifire ke typing erro se bachne ke liye ham is file me ek variable banakar use export kar denge
+
+// export const INCREMENT = "increment";
+// export const DECREMENT = "decrement";
+
+/* But ab ye karne ki jarurat nahi hai we have redux toolkit */
+
+//redux toolkit me hi redux included hota hai 
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducer: {
+        //every method receive current state and action by default
+        increment (state) {
+            // now it can allow to mutate the state
+            state.counter++;
+        },
+        decrement (state) {
+            state.decrement--;
+        },
+        //yaha pr action ki jarurat thi last two method me action ki jarurat nhi thi kyuki 1 badana ya ghatana tha But increase or decrease me kitna add ya substract karna hai nahi pata to action ki need to hogi hi.
+        increase(state, action) {
+            state.counter = state.counter+action.amount;
+        },
+        decrease(state, action) {
+            state.counter = state.counter-action.amount;
+        },
+        toggleCount(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+// we can configure more then one slice reducer me hi objectpass karke
+
+/* 
+const store = configureStore({
+    reducer: {
+        reducer1: reducer1Slice.reducer
+        reducer2: reducer2Slice.reducer
+        reducer3: reducer3Slice.reducer
+        reducer4: reducer4Slice.reducer
+    }
+});
+*/
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+
+// const counterReducer = (state = initialState, action) => {
+//     if(action.type === 'increment') {
+//         // we cant do like 
+//         /* 
+//         state.counter++ 
+//         return state
+//         */
+//        //dont try to mutate state this is rule
+//         return {
+//             counter : state.counter + 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'increase') {
+//         return {
+//             counter : state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'decrease') {
+//         return {
+//             counter: state.counter - action.amount,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'toggle'){
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter,
+//         }
+//     }
+
+//     return state;
+// }
+
+// const store = createStore(counterReducer);
 
 export default store;
