2b7cd0657de823798eb020acd5a9db5e568092f4
Devendra <devendrav1910@gmail.com>
2024-05-21 09:53:58 +0530

    building dynamic routing in nextjs

---
+++ nextjs/src/app/aboutus/developer/[id]/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function page({ params }:{ params: { id: String } }) {
+
+  const routeId = Number(params.id);
+
+  console.log(routeId);
+  const details = [
+      { id : 1, name: 'Yash', role: 'Senior Developer'},
+      { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+      { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+    ]
+
+    const data = details.filter(item => item.id === routeId)
+
+  return (
+    <div>
+      <h1>I am Devendra Vishwakarma on the developer paage</h1>
+      {data.length > 0 && ( // Check if data exists
+        <>
+          <h1>Name: {data[0].name}</h1>
+          <h2>Role: {data[0].role}</h2>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default page
