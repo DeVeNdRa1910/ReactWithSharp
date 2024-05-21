d213275548ed27fe47e4f0fb20c0698d6aae178e
Devendra <devendrav1910@gmail.com>
2024-05-21 09:57:25 +0530

    add error handling

--- nextjs/src/app/aboutus/developer/[id]/page.tsx
+++ nextjs/src/app/aboutus/developer/[id]/page.tsx
@@ -16,10 +16,14 @@ function page({ params }:{ params: { id: String } }) {
   return (
     <div>
       <h1>I am Devendra Vishwakarma on the developer paage</h1>
-      {data.length > 0 && ( // Check if data exists
+      {data.length > 0 ? ( // Check if data exists
         <>
           <h1>Name: {data[0].name}</h1>
           <h2>Role: {data[0].role}</h2>
+        </>
+      ) : (
+        <>
+          <h1>Developer doesn't exist</h1>
         </>
       )}
     </div>
